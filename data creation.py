4b3d2ecf7d44fa6877485083f707509783701c9e
Elias <eliasstalfreelancer@gmail.com>
2026-05-05 21:17:44 +0200

    added origin and data creator

---
+++ data creation.py
@@ -0,0 +1,29 @@
+import os
+import keras
+from PIL import Image
+
+# Ladda dataset
+(x_train, y_train), (x_test, y_test) = keras.datasets.fashion_mnist.load_data()
+
+# Klassnamn (viktigt för mappar)
+class_names = [
+    "T-shirt_top", "Trouser", "Pullover", "Dress", "Coat",
+    "Sandal", "Shirt", "Sneaker", "Bag", "Ankle_boot"
+]
+
+# Root folder
+base_path = "data/fashion_mnist_images"
+
+# Skapa mappar per klass
+for class_name in class_names:
+    os.makedirs(os.path.join(base_path, class_name), exist_ok=True)
+
+# Spara träningsbilder
+for i, (img, label) in enumerate(zip(x_train, y_train)):
+    class_name = class_names[label]
+    path = os.path.join(base_path, class_name, f"{i}.png")
+    
+    image = Image.fromarray(img)  # numpy → bild
+    image.save(path)
+
+print("KLAR")
